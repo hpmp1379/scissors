b0d4720d3ac462b8553cdc4389f8c8c864c4f502
Min Park <88412085+hpmp1379@users.noreply.github.com>
2022-11-17 15:27:52 +0900

    fix2

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Box = (props) => {
     //승패를 컴퓨터에 나오게
     let result;
-    if (props.title == 'Computer' && 
+    if (props.title === 'Computer' && 
         props.result !=='tie' && 
         props.result !==''){
             //컴퓨터쪽, 비기지 않았고, 결과값이 비어있지 않을때
 
-            result = props.result == 'win' ? 'lose' : 'win';
+            result = props.result === 'win' ? 'lose' : 'win';
             //내 승패여부가 승리였을때는 '패배' 아니었을때는 '승리' 로 바뀜
         } else {
             //위의 경우가 아니라면 props로 전달된 값을 그대로 사용
